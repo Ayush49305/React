a72338588a2e7e668a735f4335ae99be3ea966be
Ayush Raj <ayushrajraj12v@gmail.com>
2026-07-08 20:38:23 +0530

    Tailwind Props

---
+++ 02_Counter/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Counter Values</h1>
+      <button>Add Values</button>
+      <br></br>
+      <button>Remove Values</button>
+    </>
+  )
+}
+
+export default App
