e08928248c8ff6fd267de4acea551cbe37cd3603
Ayush Raj <ayushrajraj12v@gmail.com>
2026-07-14 14:48:48 +0530

    Component

---
+++ 02component/src/component/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className="Card">
+        <h1>Ayush Raj</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nobis?</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
