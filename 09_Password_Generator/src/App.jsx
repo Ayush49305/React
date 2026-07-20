c2717ecd42b5366999de0012bdd26569622abe26
Ayush Raj <ayushrajraj12v@gmail.com>
2026-07-20 23:01:19 +0530

    Pass Generator

--- 09_Password_Generator/src/App.jsx
+++ 09_Password_Generator/src/App.jsx
@@ -1,20 +1,87 @@
-import React from 'react'
+// import React, { useState,useCallback } from 'react'
+// const App = () => {
+
+//   const [length,setLength]=useState(8)
+//   const [numberAllowed,setNUmberAllowed]=useState(false)
+//   const [charAllowed,setCharAllowed]=useState(false)
+//   const [password,setPassword]=useState("")
+
+//   const passwordGenerator=useCallback(()=>{
+//   let pass=""
+//   let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy"
+
+//     if(numberAllowed) str+="0123456789"
+//     if(charAllowed) str+="!@#$%^&*()_+{}[]~`?/:"
+
+//     for(let i=1;i<=length;i++){
+//       let char=Math.floor(Math.random() * str.length+1)
+
+//       pass=str.charAt(char)
+//     }
+//       setPassword(pass)
+
+
+//     },[length,numberAllowed,charAllowed,setPassword])
+ 
+//   return (
+//     <>
+//         <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-500'>Test
+
+//         </div>
+      
+//     </>
+//   )
+// }
+
+// export default App
+
+import React, { useState, useCallback, useEffect } from "react";
 
 const App = () => {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  // Password Generator
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*()_+{}[]~`?/:";
+
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed]);
+
+  //Generate password when dependencies change
+  useEffect(() => {
+    passwordGenerator();
+  }, [passwordGenerator]);
+
   return (
-    
-        <div className='h-full w-screen text-4xl'>
+  <div className='w-full h-screen bg-black flex justify-center'>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 text-orange-500 bg-gray-700'>
+      <h1 className='text-white text-center text-2xl my-3'>Password Generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input 
+        type="text"
+        value={password}
+        className='outline-none w-full py-1 px-3 bg-white'
+        placeholder='password'
+        readOnly
+         />
 
-          <div className='h-1/12 w-2xl flex justify-between items-center px-20 w-4xl bg-red-400 mb-16 py-16'>
-            {/* <input className='h-12 w-3xl rounded-4xl bg-green-500 flex justify-between items-center mt-100'type="text" placeholder='generate Password' />
-            <h1>COPY</h1> */}
+      </div>
 
-          </div>
-
-        </div>
-      
-    
+     </div>
+  </div>
   )
 }
 
-export default App
+export default App;
